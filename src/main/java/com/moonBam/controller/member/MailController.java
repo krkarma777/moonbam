f2053ec99007de784f197c88823fc561c40800f2
Haru <Haru@DESKTOP-L06C1RU>
2024-03-12 15:35:47 +0900

    chore : 이메일 시스템을 resoures의 파일과 연결

--- src/main/java/com/moonBam/controller/member/MailController.java
+++ src/main/java/com/moonBam/controller/member/MailController.java
@@ -1,5 +1,8 @@
 package com.moonBam.controller.member;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,29 +33,54 @@ public class MailController {
 
 	//단순 메일 전송
     public void sendEmail(String userEmail, MemberDTO dto) throws Exception {
-        	String TO_EMAIL = userEmail;
+    	
+    		//Resources 폴더부터 경로 설정
+    		ClassPathResource resource = new ClassPathResource("static/emailFiles/PWEmail.html");
+    		//BufferedReader를 통해 한줄씩 읽어옴 || InputStreamReader를 통해 byte를 String Stream으로 변경
+    		BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+    		//StringBuilder: 문자열 연산을 수행할 때마다, 기존 문자열에 변경사항을 반영하여 작업
+    		StringBuilder emailBody = new StringBuilder();
+
+    		String line;
+    		//HTML의 각 줄을 읽어오고, 특정 글자는 치환
+    		while ((line = br.readLine()) != null) {
+    		    line = line.replace("##유저_아이디##", dto.getUserId())
+    		               .replace("##유저_이름##", dto.getUserName())
+    		               .replace("##유저_비밀번호##", dto.getUserPw());
+    		    //각 줄을 StringBuilder에 더하고, 개행
+    		    emailBody.append(line).append("\n");
+    		}
+
+    		String TO_EMAIL = userEmail;
         	String EMAIL_SUBJECT = "[회원정보알림] 문화인의 밤을 이용해주셔서 감사합니다.";
-        	String EMAIL_BODY = 
-        			"<p>안녕하세요? 문화인의 밤입니다.</p><br>"
-					+ "<p>"+dto.getUserName()+"님의 아이디는 "+dto.getUserId()+", 비밀번호는 "+dto.getUserPw()+"입니다.</p><br>"
-					+ "<p style='text-align: right;'>감사합니다.<br>"
-					+ "문화인의 밤 드림.</p>";
+        	
+        	//StringBuilder를 String으로 전환하고 이메일 본문으로 저장
+        	String EMAIL_BODY = emailBody.toString();
+
             serv.sendEmail(FROM_EMAIL, TO_EMAIL, EMAIL_SUBJECT, EMAIL_BODY);
     }
     
     //단순 메일 전송
     @PostMapping("/joinEmail")
     public String joinEmail(String userEmail) throws Exception {
-    		
+    	
     		String authNumber = makeRandomNumber();
+    	
+			ClassPathResource resource = new ClassPathResource("static/emailFiles/JoinEmail.html");
+			BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+			StringBuilder emailBody = new StringBuilder();
+	
+			String line;
+			while ((line = br.readLine()) != null) {
+			    line = line.replace("##인증번호##", authNumber)
+			               .replace("##인증번호_유효기간##", expireDate);
+			    emailBody.append(line).append("\n");
+		}
+    	
         	String TO_EMAIL = userEmail;
         	String EMAIL_SUBJECT = "[회원정보알림] 문화인의 밤을 이용해주셔서 감사합니다.";
-        	String EMAIL_BODY = 
-        			"<p>안녕하세요? 문화인의 밤입니다.</p><br>"
-					+ "<p>인증 번호는 [ " + authNumber + " ] 입니다.</p>" 
-					+ "<p>인증번호의 유효기간은 [ " + expireDate + " ] 입니다.</p><br>"
-					+ "<p style='text-align: right;'>감사합니다.<br>"
-					+ "문화인의 밤 드림.</p>";
+        	String EMAIL_BODY = emailBody.toString();
+        			
             serv.sendEmail(FROM_EMAIL, TO_EMAIL, EMAIL_SUBJECT, EMAIL_BODY);
             return "send";
     }
