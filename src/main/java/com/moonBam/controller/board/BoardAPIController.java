adf4c99530afdff2826291a7b451d66bc992b4c5
hhee3 <hhee3@DESKTOP-24HMD7R>
2024-03-31 01:23:50 +0900

    postId를 이용하여 해당되는 게시판 데이터를 화면에 뿌려주기, 글쓰기 화면인 post.jsp에서 임시저장 ajax 요청 주소 수정

--- src/main/java/com/moonBam/controller/board/BoardAPIController.java
+++ src/main/java/com/moonBam/controller/board/BoardAPIController.java
@@ -1,20 +1,36 @@
 package com.moonBam.controller.board;
 
-import com.moonBam.dto.MemberDTO;
-import com.moonBam.dto.board.PostDTO;
-import com.moonBam.dto.board.PostUpdateRequestDTO;
-import com.moonBam.service.PostService;
-import lombok.RequiredArgsConstructor;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.moonBam.dto.MemberDTO;
+import com.moonBam.dto.board.PageDTO;
+import com.moonBam.dto.board.PostDTO;
+import com.moonBam.dto.board.PostPageDTO;
+import com.moonBam.dto.board.PostUpdateRequestDTO;
+import com.moonBam.service.PostService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -98,5 +114,66 @@ public class BoardAPIController {
 
         return ResponseEntity.ok(Map.of("message", "삭제가 완료되었습니다."));
     }
+    
+    
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> findOne(@PathVariable("postId") Long postId, HttpSession session){
+    	PostPageDTO pDTO = postService.selectPagePost(postId);
+    	
+    	if (postId == null) {
+    		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "존재하지 않는 게시글입니다."));
+    	}
+    	
+    	MemberDTO loginUser = (MemberDTO)session.getAttribute("loginUser");
+        boolean isAuthorized = loginUser.getUserId().equals(pDTO.getUserId());
+    	
+    	return ResponseEntity.ok(Map.of("pDTO", pDTO, "isAuthorized", isAuthorized));
+    }
+    
+    //boardId에 따라 postList 가져오기
+    @PatchMapping
+    public ResponseEntity<?> findAll(
+    		@PathVariable("postBoard") String postBoard,
+    		@RequestParam(value = "curPage", defaultValue = "1") int curPage,
+            @RequestParam(value = "postCategoryId", required = false) String postCategoryId,
+            @RequestParam(value = "searchParam", required = false) String searchParam,
+            @RequestParam(value = "sortParam", required = false) String sortParam, Model model
+    		) {
+    	
+        int perPage = 20;
+        int offset = (curPage - 1) * perPage;
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("postBoard", postBoard);
+        map.put("offset", offset);
+        map.put("perPage", perPage);
+        map.put("curPage", curPage);
+        map.put("postCategoryId", postCategoryId);
+        map.put("searchParam", searchParam);
+        map.put("sortParam", sortParam);
+
+        // 검색 조건 추가
+        // searchBoard(paramMap, map);
+
+        // 정렬 추가
+        // sortIndex(paramMap, map);
+
+        // 페이지 정보 가져오기
+        PageDTO<PostPageDTO> pageDTO = postService.getPostsByPage(map);
+
+        List<PostPageDTO> hotList = postService.selectAll((HashMap<String, String>) Map.of("board", postBoard, "postCount", "5"));
+
+        List<PostPageDTO> popularListAll = postService.popularPostTwoDays((HashMap<String, String>) Map.of("postCount", "10"));
+        
+		final String category = postBoard.contains("movie") ? "movie" : 
+            postBoard.contains("tv") ? "tv" : 
+            postBoard.contains("book") ? "book" : "";
+		
+		List<PostPageDTO> popularListCategory = postService.popularPostTwoDays(
+				(HashMap<String, String>) Map.of("board", category, "board2", category+"Info", "board3", category+"Meet", "postCount", "10"));
+    	    	
+    	return ResponseEntity.ok(Map.of("pageDTO", pageDTO, "postBoard", postBoard, "hotList", hotList, 
+    			"popularListAll", popularListAll, "popularListCategory",popularListCategory));
+	}
 }
 
