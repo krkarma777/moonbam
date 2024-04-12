eceebda52d3f7b20899fbfedffdfa4075c543747
user <user@BOOK-UP5F0RS3G7>
2024-04-12 14:53:55 +0900

    chat-more 하는중 . ChatRoomDTO 수정했음 (mdata추가, count변수명 변경)

---
+++ src/main/java/com/moonBam/service/CommunityChatmoreService.java
@@ -0,0 +1,41 @@
+package com.moonBam.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moonBam.dao.CommunityChatmoreDAO;
+import com.moonBam.dto.ChatMemberDTO;
+import com.moonBam.dto.MemberDTO;
+import com.moonBam.dto.ReportDTO;
+
+@Service
+public class CommunityChatmoreService {
+	
+	@Autowired
+	CommunityChatmoreDAO chatMoreDao;
+	
+	public List<String> ChatMemberIdByChatNum(int chatNum){
+		return chatMoreDao.ChatMemberIdByChatNum(chatNum);
+	}
+
+	public MemberDTO memberByChatMemberId(String userId) {
+		// TODO Auto-generated method stub
+		return chatMoreDao.memberByChatMemberId(userId);
+	}
+
+	public String ChatLeaderIdByChatNum(int chatNum) {
+		// TODO Auto-generated method stub
+		return chatMoreDao.ChatLeaderIdByChatNum(chatNum);
+	}
+
+	public void ChatmoreReportPostInsert(ReportDTO reportDTO) {
+		// TODO Auto-generated method stub
+		chatMoreDao.ChatmoreReportPostInsert(reportDTO);
+	}
+	
+	
+	
+
+}
