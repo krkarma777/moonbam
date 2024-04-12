8f4a8ee192803ba229a9083bdc5b05ef602dbf0e
hhee3 <hhee3@DESKTOP-24HMD7R>
2024-04-12 12:27:21 +0900

    채팅방 개설

--- src/main/java/com/moonBam/controller/community/ChatController.java
+++ src/main/java/com/moonBam/controller/community/ChatController.java
@@ -3,13 +3,23 @@ package com.moonBam.controller.community;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.moonBam.dto.ChatRoomDTO;
 
 @Controller
 public class ChatController {
 	
 	@RequestMapping(value = "/createChat", method = RequestMethod.GET)
-	public String createChat() {
+	public String createChatForm() {
 		return "/community/createChat";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/createChat", method = RequestMethod.POST)
+	public String createChat(ChatRoomDTO dto) {
+		System.out.println(dto);
+		return "모임 만들기 성공";
 	}
 	
 	
