cc89c1fdc067fe54a40ae54da172aa59659513f1
user <user@BOOK-UP5F0RS3G7>
2024-04-15 15:11:59 +0900

    24-04-15 1511 jay

--- src/main/java/com/moonBam/controller/community/oh/ChatRoomsDAO.java
+++ src/main/java/com/moonBam/controller/community/oh/ChatRoomsDAO.java
@@ -19,39 +19,23 @@ public class ChatRoomsDAO {
 //        return session.update("ChatRoomMapper.update", requestDTO);
 //    }
 //
-//import java.util.List;
-//
-//@Repository
-//@RequiredArgsConstructor
-//public class ChatRoomsDAO {
-//
-//    private final SqlSessionTemplate session;
-//
-//    public int create(ChatRoomCreateRequestDTO requestDTO) {
-//        return 1;
+//    public int delete(int id) {
+//        return session.delete("ChatRoomMapper.delete", id);
 //    }
 //
-////    public int update(ChatRoomUpdateRequestDTO requestDTO) {
-////        return session.update("ChatRoomMapper.update", requestDTO);
-////    }
-////
-////    public int delete(int id) {
-////        return session.delete("ChatRoomMapper.delete", id);
-////    }
-////
-////    public List<ChatRoomDTO> selectAll() {
-////        return session.selectList("ChatRoomMapper.selectAll");
-////    }
-////
-////    public List<ChatRoomDTO> selectByCategory(String category) {
-////        return session.selectList("ChatRoomMapper.selectByCategory", category);
-////    }
-////
-////    public ChatRoomDTO selectById(int id) {
-////        return session.selectOne("ChatRoomMapper.selectById", id);
-////    }
-////
-////    public ChatRoomDTO selectByLocation(String location) {
-////        return session.selectOne("ChatRoomMapper.selectByLocation", location);
-////    }
-//}
+//    public List<ChatRoomDTO> selectAll() {
+//        return session.selectList("ChatRoomMapper.selectAll");
+//    }
+//
+//    public List<ChatRoomDTO> selectByCategory(String category) {
+//        return session.selectList("ChatRoomMapper.selectByCategory", category);
+//    }
+//
+//    public ChatRoomDTO selectById(int id) {
+//        return session.selectOne("ChatRoomMapper.selectById", id);
+//    }
+//
+//    public ChatRoomDTO selectByLocation(String location) {
+//        return session.selectOne("ChatRoomMapper.selectByLocation", location);
+//    }
+}
