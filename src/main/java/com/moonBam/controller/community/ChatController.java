d1a566265d6654e32cba5d9ac5b8291d300f5ae7
kwangr1995 <kwangr1995@gmail.com>
2024-04-18 16:25:06 +0900

    채팅 강퇴 관련 변경

--- src/main/java/com/moonBam/controller/community/ChatController.java
+++ src/main/java/com/moonBam/controller/community/ChatController.java
@@ -126,7 +126,19 @@ public class ChatController {
 	
 	@RequestMapping(value = "/ChatKickUser", method = RequestMethod.GET)
 	public String ChatKickUser (String user) {
-		int n = crService.ChatKickUser(user);
+		
+		int n = 0;
+		
+		System.out.println("ChatKickUser===================");
+		System.out.println("강퇴할 유저의 이름 확ㅇ인 후 서비스레이어 전달");
+		System.out.println(user);
+		System.out.println("=================================");
+		
+		n = crService.ChatKickUser(user);
+		
+		System.out.println("1이면 정상처리됨==========");
+		System.out.println(n);
+		System.out.println("====================");
 		
 		return "";
 	}
